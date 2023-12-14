7aa8f64e001d98c6530bf050d781dbb77f39996d
rakesh <rakeshsahoo5954@gmail.com>
2023-12-14 17:45:09 +0530

    first commit

---
+++ src/main/java/nopCommerceV2/nopCommerceV2/App.java
@@ -0,0 +1,13 @@
+package nopCommerceV2.nopCommerceV2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
